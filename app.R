26dff197447bfb72d9640ea7f76cfb6eb5207337
Varsha Bhat <82274960+vtbhat@users.noreply.github.com>
2022-11-22 11:02:53 -0500

    Edited the file options

--- app.R
+++ app.R
@@ -1,3 +1,5 @@
+library(BiocManager)
+options(repos = BiocManager::repositories())
 library(shiny)
 library(shinythemes)
 library(dplyr)
@@ -6,6 +8,8 @@ library(scATAC.Explorer)
 library(scRNAseq)
 library(shinyjs)
 library(DT)
+library(rsconnect)
+
 
 atacdata_table<-as.data.frame(queryATAC(metadata_only=TRUE))
 accession_atac<-as.vector(unique(atacdata_table$Accession))
@@ -23,16 +27,29 @@ ui <- fluidPage(theme=shinytheme("yeti"),
                 h4("You can upload your own data or analyze publcily available data"),
                 sidebarLayout (
                   sidebarPanel(
-                    actionButton('upload_btn', 'Upload Datasets',
-                                 style="color: #fff; background-color: #337ab7; border-color: #2e6da4; text-align: center;"),
+                    h3(HTML("Upload datasets"), style={'text-align: center;'}),
+                    h5(HTML("Upload scATAC-Seq data"), style={'text-align: center; font-weight: bold;'}),
+                    fileInput("atacdata", "Choose scATAC-Seq fragments.tsv File", accept = ".tsv.gz"),
+                    fileInput("atacdata", "Choose scATAC-Seq H5 File", accept = ".tsv.gz"),
+                    h5(HTML("Upload scRNA-Seq data"), style={'text-align: center; font-weight: bold;'}),
+                    fileInput("scrnadata", "Choose scRNA-Seq RDS/H5AD/Matrix File"),
+                    fileInput("scrnadata", "Choose scRNA-Seq Features File"),
+                    fileInput("scrnadata", "Choose scRNA-Seq Barcode File"),
+                    actionButton('integrateupload_btn', 'Analyze and Integrate',
+                                 style="color: #fff; background-color: #4CAF50; text-align: center"),
                     h4(HTML("OR"), style={'text-align: center;'}),
-                    actionButton('select_btn', 'Select Public Datasets',
-                                 style="color: #fff; background-color: #337ab7; border-color: #2e6da4; text-align: center"),
+                    h3(HTML("Select publicly available datasets"), style={'text-align: center;'}),
                     h4(HTML("Click the buttons below to see available datasets"), style={'text-align: center;'}),
                     actionButton('atac_public', 'Show scATAC-Seq Datasets',
                                  style="text-align: center"),
                     actionButton('scrna_public', 'Show scRNA-Seq Datasets',
-                                 style="text-align: center")
+                                 style="text-align: center"),
+                    selectInput("atac_public_data", "Choose the scATAC-Seq dataset of interest",
+                                choices=accession_atac),
+                    selectInput("scrna_public_data", "Choose the scRNA-Seq dataset of interest",
+                                choices=accession_scrna),
+                    actionButton('integratepublic_btn', 'Analyze and Integrate',
+                                 style="color: #fff; background-color: #4CAF50; text-align: center")
                   ),
                   mainPanel(
                     DT::dataTableOutput("mytable")
@@ -46,18 +63,7 @@ ui <- fluidPage(theme=shinytheme("yeti"),
 ######Server
 server <- function(input, output) {
   #To upload user datasets
-  observeEvent(input$upload_btn, {
-    insertUI(
-      selector = "#upload_btn",
-      where = "afterEnd",
-      ui = tagList(fileInput("atacdata", "Choose scATAC-Seq File", accept = ".tsv.gz"),
-                   fileInput("scrnadata", "Choose scRNA-Seq RDS File"),
-                   actionButton('integrateupload_btn', 'Analyze and Integrate',
-                                style="color: #fff; background-color: #4CAF50; text-align: center"))
-      
-    )
-    
-  })
+  
   
   #To show public scATAC-Seq datasets
   observeEvent(input$atac_public, {
@@ -67,6 +73,7 @@ server <- function(input, output) {
     caption=htmltools::tags$caption("scATAC-Seq Datasets", style="color:black; font-size: 20px; font-weight:bold"))
   })
   
+  
   #To show public scRNA-Seq datasets
   observeEvent(input$scrna_public, {
     output$mytable = DT::renderDataTable({
@@ -75,18 +82,6 @@ server <- function(input, output) {
     caption=htmltools::tags$caption("scRNA-Seq Datasets", style="color:black; font-size: 20px; font-weight:bold"))
   })
   
-  #To select public datasets
-  observeEvent(input$select_btn,{
-    insertUI(
-      where = "afterEnd",
-      ui = tagList(selectInput("atac_public_data", "Choose the scATAC-Seq dataset of interest",
-                               choices=accession_atac),
-                   selectInput("scrna_public_data", "Choose the scRNA-Seq dataset of interest",
-                               choices=accession_scrna),
-                   actionButton('integratepublic_btn', 'Analyze and Integrate',
-                                style="color: #fff; background-color: #4CAF50; text-align: center"))
-    )
-  })
   
 }
 
